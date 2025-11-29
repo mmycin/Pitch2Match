6bb39f1ae748d70724a89b926b240f4b7522a295
Tahcin Ul Karim Mycin <150533238+mmycin@users.noreply.github.com>
2025-11-29 23:23:59 +0600

    Backend API only

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/api/health', function () {
+    return response()->json(['message' => 'API only backend']);
 });
